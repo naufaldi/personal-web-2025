7db614ce2e248a0295a948b199c65df4b4af77df
naufaldi <naufaldi.rafif@gmail.com>
2025-11-13 15:08:55 +0700

    feat: add ScrollToTop component to enhance user experience by scrolling to the top on route changes

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router'
 import Header from './Header'
 import Footer from './Footer'
+import ScrollToTop from './ScrollToTop'
 import ConnectCTA from '@/components/sections/ConnectCTA'
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />
